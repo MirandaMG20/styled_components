b885a4c5888f361ef1c868402764a3208d334201
MirandaMG20 <guadamiran@gmail.com>
2023-07-28 18:50:12 -0400

    Finished

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,6 @@ import { StyledCard } from "./styles/Card.styled";
 export default function Card({ item: { id, title, body, image } }) {
   return (
     <StyledCard layout={id % 2 === 0 && "row-reverse"}>
-      <div>
         <div>
           <h2>{title}</h2>
           <p>{body}</p>
@@ -12,7 +11,6 @@ export default function Card({ item: { id, title, body, image } }) {
         <div>
           <img src={`./images/${image}`} alt="" />
         </div>
-      </div>
     </StyledCard>
   );
 }
